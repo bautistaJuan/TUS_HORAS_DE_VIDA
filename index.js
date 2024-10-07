efa5f533ec556c5afa58465a1b9fc6923a3886ca
Juan Bautista Vega <jv604824@gmail.com>
2024-10-07 11:24:58 -0300

    Added effects

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const result = document.getElementById("result");
+const loader = document.getElementById("loader");
+
 const btn = document.getElementById("calculateBtn");
 btn.addEventListener("click", function () {
   const birthDate = document.getElementById("birthDate").value;
+  result.textContent = "";
 
   if (!birthDate) {
     alert("Por favor, selecciona tu fecha de nacimiento.");
@@ -8,13 +12,17 @@ btn.addEventListener("click", function () {
   }
 
   const horasVividas = calcularHorasVividas(new Date(birthDate));
-  const result = document.getElementById("result");
-  result.textContent = `¡Has vivido aproximadamente ${horasVividas.toLocaleString()} horas! 🎉`;
+  loader.style.display = "grid";
+
+  setTimeout(() => {
+    result.textContent = `¡Has vivido aproximadamente ${horasVividas.toLocaleString()} horas! 🎉`;
+    loader.style.display = "none";
+  }, 1500);
 });
 
 function calcularHorasVividas(fechaNacimiento) {
   const hoy = new Date(); // Fecha actual
   const diferencia = hoy.getTime() - fechaNacimiento.getTime(); // Diferencia en milisegundos
-  const horas = diferencia / (1000 * 60 * 60); // Convertir a horas
-  return Math.floor(horas); // Redondear hacia abajo
+  const horas = diferencia / (1000 * 60 * 60); // Convierte a horas
+  return Math.floor(horas); // Redondea hacia abajo
 }
