6e84cd32d201feb2189510f9c47980df5e169ef1
Juan Bautista Vega <jv604824@gmail.com>
2024-10-07 12:08:52 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const result = document.getElementById("result");
 const loader = document.getElementById("loader");
-
 const btn = document.getElementById("calculateBtn");
+
 btn.addEventListener("click", function () {
-  const birthDate = document.getElementById("birthDate").value;
   result.textContent = "";
+  const birthDate = document.getElementById("birthDate").value;
 
   if (!birthDate) {
     alert("Por favor, selecciona tu fecha de nacimiento.");
     return;
   }
-
-  const horasVividas = calcularHorasVividas(new Date(birthDate));
   loader.style.display = "grid";
-
+  const horasAprox = vividasOPorVivir(birthDate); // Esta constante guarda el texto que retorna vividasOPorVivir
   setTimeout(() => {
-    result.textContent = `¡Has vivido aproximadamente ${horasVividas.toLocaleString()} horas! 🎉`;
+    result.textContent = horasAprox;
     loader.style.display = "none";
   }, 1500);
 });
 
-function calcularHorasVividas(fechaNacimiento) {
+function calcularHoras(fechaNacimiento) {
   const hoy = new Date(); // Fecha actual
   const diferencia = hoy.getTime() - fechaNacimiento.getTime(); // Diferencia en milisegundos
   const horas = diferencia / (1000 * 60 * 60); // Convierte a horas
   return Math.floor(horas); // Redondea hacia abajo
+}
+
+function vividasOPorVivir(birthDate) {
+  const horasVividas = calcularHoras(new Date(birthDate));
+  let hourAbsolute = Math.abs(horasVividas);
+  if (horasVividas < 0) {
+    return `¡Te quedan ${hourAbsolute.toLocaleString()} horas para llegar a ese día! 🎉`;
+  } else {
+    return `¡Desde esa fecha has vivido aproximadamente ${hourAbsolute.toLocaleString()} horas! 🎉`;
+  }
 }
